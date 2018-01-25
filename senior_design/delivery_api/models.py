d17bdab3d6b39aa116776f6838b0ee57ba583bc6
Dane Sowers <sowersdd@mail.uc.edu>
2018-01-25 17:44:23 -0500

    Got project working, added Destination model

--- senior_design/delivery_api/models.py
+++ senior_design/delivery_api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Destination(models.Model):
+	id = models.IntegerField(primary_key=True)
+	created = models.DateTimeField(auto_now_add=True)
+	lat = models.CharField(max_length=50)
+	lon = models.CharField(max_length=50)
+	pending = models.BooleanField(default=True)
+
+	class Meta:
+	    ordering = ('created', )
--- senior_design/delivery_api/views.py
+++ senior_design/delivery_api/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from senior_design.delivery_api.models import Destination
+from rest_framework import viewsets
+from senior_design.delivery_api.serializers import UserSerializer, GroupSerializer, DestinationSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+class DestinationViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Destination.objects.all()
+    serializer_class = DestinationSerializer
