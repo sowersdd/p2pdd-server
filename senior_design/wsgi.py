ba8db9f6c80bb12e93405c7fd9c1f105a4be6fed
Dane Sowers <sowersdd@mail.uc.edu>
2018-02-23 16:32:37 -0500

    Updates

--- senior_design/wsgi.py
+++ senior_design/wsgi.py
@@ -8,8 +8,11 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
-
+from dotenv import load_dotenv
 from django.core.wsgi import get_wsgi_application
+
+project_folder = os.path.expanduser('/var/www/p2pdd-server')  # adjust as appropriate
+load_dotenv(os.path.join(project_folder, '.env'))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "senior_design.settings")
 
